2c4fe83376be74e7444d9ef06565d7ea65206d43
yzm5tj <yzm5tj@gm.com>
2019-01-29 15:36:14 -0500

    Added project

---
+++ app/src/main/java/mauricee/me/dreamscape/BasePresenter.kt
@@ -0,0 +1,34 @@
+package me.mauricee.pontoon
+
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
+import me.mauricee.pontoon.analytics.EventTracker
+
+abstract class BasePresenter<S : EventTracker.State, in V : BaseContract.View<S, *>>(internal val eventTracker: EventTracker)
+    : BaseContract.Presenter<V> {
+
+    private val subs = CompositeDisposable()
+
+    final override fun attachView(view: V) {
+        subs += onViewAttached(view).observeOn(AndroidSchedulers.mainThread())
+                .doOnNext { eventTracker.trackState(it, view) }
+                .doOnSubscribe { eventTracker.trackStart(view) }
+                .doOnDispose { eventTracker.trackStop(view) }
+                .distinctUntilChanged()
+                .subscribe(view::updateState)
+    }
+
+    final override fun detachView() {
+        subs.clear()
+        onViewDetached()
+    }
+
+    inline fun stateless(crossinline action: () -> Unit): Observable<S> =
+            Observable.defer { action(); Observable.empty<S>() }
+
+    protected open fun onViewAttached(view: V): Observable<S> = Observable.empty()
+
+    protected open fun onViewDetached() {}
+}
